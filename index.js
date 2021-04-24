81886cba1a157a6451cc65b108455e4927d1e15e
Tom <thomas.spencer@codurance.com>
2021-04-24 13:05:31 +0100

    Add iterator pattern

---
+++ index.js
@@ -0,0 +1,30 @@
+#!/usr/bin/env node
+
+class MyContainer {
+  [Symbol.iterator]() {
+    let counter = 0;
+    return {
+      next() {
+        return {
+          done: counter > 5,
+          value: counter++
+        };
+      }
+    }
+  }
+}
+
+const myObject = new MyContainer();
+
+for (let element of myObject){
+  console.log(element);
+}
+
+let iterator = myObject[Symbol.iterator]();
+let value = iterator.next();
+console.log(value.done);
+
+while (!iterator.done){
+  console.log(value.value);
+  value = iterator.next();
+}
